abb33f223dfc2ad80a0644337934b40f6b34ff14
EC2 Default User <ec2-user@ip-172-31-37-102.ec2.internal>
2021-11-21 22:28:31 +0000

    header error now

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,3 +1,4 @@
 class HomesController < ApplicationController
-
+  def new
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def index
+    @books = @user.books.all
+  end
+  def show
+    @user = User.new(user_params)
+    @user = User.find(params[:id])
+  end
+
   def edit
     @user = User.find(params[:id])
   end
 
-  def show
-    @user = User.find(params[:id])
-    @books = @user.books.all
-  end
-
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to book_path
   end
 end
